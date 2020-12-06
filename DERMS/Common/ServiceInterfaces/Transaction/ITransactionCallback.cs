20daf70c5348d0956b83af7e0aef3e93693955ee
Aleksandar Jovanov <jovanov33@gmail.com>
2020-12-06 12:59:46 +0100

    [Common] Pre-transaction interface added
    Added Model promotion participant interface which is used as a pre-procedure for the upcoming transaction.
    Added comments on transaction related interfaces.

--- DERMS/Common/ServiceInterfaces/Transaction/ITransactionCallback.cs
+++ DERMS/Common/ServiceInterfaces/Transaction/ITransactionCallback.cs
@@ -2,15 +2,30 @@
 
 namespace Common.ServiceInterfaces.Transaction
 {
+    /// <summary>
+    /// Used for components which participate in transaction.
+    /// </summary>
     [ServiceContract]
     public interface ITransactionCallback
     {
+        /// <summary>
+        /// Perform prepare phase.
+        /// </summary>
+        /// <returns><b>True</b> if the prepared phase goes trough, otherwise <b>false</b>.</returns>
         [OperationContract]
         bool Prepare();
 
+        /// <summary>
+        /// Perform commit phase.
+        /// </summary>
+        /// <returns><b>True</b> if the commit phase goes trough, otherwise <b>false</b>.</returns>
         [OperationContract]
         bool Commit();
 
+        /// <summary>
+        /// Perform prepare phase.
+        /// </summary>
+        /// <returns><b>True</b> if the rollback phase goes trough, otherwise <b>false</b>.</returns>
         [OperationContract]
         bool Rollback();
     }
--- DERMS/Common/ServiceInterfaces/Transaction/ITransactionManager.cs
+++ DERMS/Common/ServiceInterfaces/Transaction/ITransactionManager.cs
@@ -2,15 +2,32 @@
 
 namespace Common.ServiceInterfaces.Transaction
 {
+    /// <summary>
+    /// Transaction managers interface.
+    /// </summary>
     [ServiceContract(CallbackContract = typeof(ITransactionCallback))]
     public interface ITransactionManager
     {
+        /// <summary>
+        /// Starts the enlist procedure.
+        /// </summary>
+        /// <returns><b>True</b> if the start enlist procedure is performed. If the trasaction is already started, <b>false</b> will be returned.</returns>
         [OperationContract]
         bool StartEnlist();
 
+        /// <summary>
+        /// Enlists the service in the ongoing transaction.
+        /// </summary>
+        /// <param name="serviceName">Name of the service which wants to participate in the transaction.</param>
+        /// <returns><b>True</b> if the service is sucessfuly enlisted, otherwise <b>false</b>.</returns>
         [OperationContract]
         bool EnlistService(string serviceName);
 
+        /// <summary>
+        /// Ends the enlist procedure.
+        /// </summary>
+        /// <param name="allServicesEnlisted"><b>True</b> if all ment services are enlisted, otherwise <b>false</b> which forces transaction into <b>Rollback</b> phase.</param>
+        /// <returns><b>True</b> if the procedure is successfuly executed, otherwise <b>false</b>.</returns>
         [OperationContract]
         bool EndEnlist(bool allServicesEnlisted);
     }
