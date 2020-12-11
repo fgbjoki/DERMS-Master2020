bdec9cb58a7f78321a5ffbae070be7593c9d7017
Aleksandar Jovanov <jovanov33@gmail.com>
2020-12-11 20:23:44 +0100

    [NMS] Domain model classes added (#9)
    * [NMS] Added DataModel classes
    
    Added DataModel classes based on EAP model.
    Todo: implement each class
    
    * [Common] Added ModelCodes and Enums
    
    Added all neccessary ModelCodes and enums defined in model.
    
    * [Common] ModelResourceDesc now has defined logic for given domain
    
    Added insert order into Delta and not settable properties.
    
    * [Common] Added Enum to ModelCode conversion.

---
+++ DERMS/NetworkManagementService/DataModel/DER Specific/DistributedEnergyResource.cs
@@ -0,0 +1,14 @@
+﻿using NetworkManagementService.DataModel.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkManagementService.DataModel.DER_Specific
+{
+    public class DistributedEnergyResource : ConductingEquipment
+    {
+        // TODO
+    }
+}
---
+++ DERMS/NetworkManagementService/DataModel/Measurement/Measurement.cs
@@ -0,0 +1,14 @@
+﻿using NetworkManagementService.DataModel.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkManagementService.DataModel.Measurement
+{
+    public class Measurement : IdentifiedObject
+    {
+        // TODO
+    }
+}
