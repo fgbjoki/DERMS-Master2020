289f888ab81d423f28109ae1bdfb41d8b7e574d2
Aleksandar Jovanov <jovanov33@gmail.com>
2020-12-11 18:35:11 +0100

    [Common] Added ModelCodes and Enums
    Added all neccessary ModelCodes and enums defined in model.

--- DERMS/Common/AbstractModel/Enums.cs
+++ DERMS/Common/AbstractModel/Enums.cs
@@ -1,10 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Common.AbstractModel
+﻿namespace Common.AbstractModel
 {
-    // TODO
+    public enum MeasurementType : short
+    {
+        ActiveEnergy = 0,
+        ActivePower = 1,
+        Discrete = 2,
+        Humidity = 3,
+        Money = 4,
+        Percet = 5,
+        SkyCover = 7,
+        Status = 8,
+        SunshineMinutes = 9,
+        Temperature = 10,
+        Time = 11,
+        Unitless = 12,
+        WindSpeed = 13
+    }
+
+    public enum SignalDirection: short
+    {
+        Read = 1,
+        Write = 2,
+        ReadWrite = 3,
+    }
+
+    public enum EnergyStorageState : short
+    {
+        Idle = 0,
+        Charging = 1,
+        Discharging = 2
+    }
 }
