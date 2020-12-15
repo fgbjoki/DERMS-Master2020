185ab65e8f44294dbf225bc85cc4233b89ce7193
Aleksandar Jovanov <jovanov33@gmail.com>
2020-12-15 20:01:00 +0100

    [Importer] Added CIMXML classes
    Added CIMXML classes.

--- DERMS/Importer/CIMAdapter/Manager/ProfileManager.cs
+++ DERMS/Importer/CIMAdapter/Manager/ProfileManager.cs
@@ -5,7 +5,6 @@ namespace FTN.ESI.SIMES.CIM.CIMAdapter.Manager
 {
 	public enum SupportedProfiles : byte
 	{
-		PowerTransformer = 0,
 		DERMS
 	};
 
@@ -15,7 +14,7 @@ namespace FTN.ESI.SIMES.CIM.CIMAdapter.Manager
 	/// </summary>
 	public static class ProfileManager
 	{
-		public const string Namespace = "FTN";
+		public const string Namespace = "DERMS";
 
 		/// <summary>
 		/// Method returns the name of CIM profile based on the defined enumeration.
