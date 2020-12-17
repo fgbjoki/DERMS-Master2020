dab2a1e479a6ec918ffbb2af8b438a93aadff57b
Aleksandar Jovanov <jovanov33@gmail.com>
2020-12-17 13:33:41 +0100

    [Common] Added WCFClient and WCFDuplexClient
    Implemented generic wcf client and wcf duplex client.

---
+++ DERMS/Common/Communication/WCFClient.cs
@@ -0,0 +1,18 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+namespace Common.Communication
+{
+    public class WCFClient<T> : ChannelFactory<T>
+    {
+        public WCFClient(Binding binding, EndpointAddress remoteAddress) : base(binding, remoteAddress)
+        {
+
+        }
+
+        public T Proxy
+        {
+            get { return CreateChannel(); }
+        }
+    }
+}
