f7d0ce22cd76c56fca071d73b821b082e39ef65f
Aleksandar Jovanov <jovanov33@gmail.com>
2020-12-26 23:00:03 +0100

    [NMS] NMS starts transaction when delta is valid
    NMS now has logic to start transaction when incomming delta is valid.

--- DERMS/NetworkManagementService/Program.cs
+++ DERMS/NetworkManagementService/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.ServiceModel;
+using System.ServiceModel.Configuration;
 using System.Text;
 using System.Threading.Tasks;
 
