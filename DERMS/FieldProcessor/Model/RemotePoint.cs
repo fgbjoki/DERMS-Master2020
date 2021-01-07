76b3fe280e6e809a737d43c67586e56d3976fe52
Aleksandar Jovanov <jovanov33@gmail.com>
2021-01-07 11:27:17 +0100

    [FieldProcessor] Added RemotePoint model

---
+++ DERMS/FieldProcessor/Model/RemotePoint.cs
@@ -0,0 +1,18 @@
+﻿namespace FieldProcessor.Model
+{
+    public class RemotePoint
+    {
+        public RemotePoint(long gid, ushort address, RemotePointType type)
+        {
+            Gid = gid;
+            Address = address;
+            Type = type;
+        }
+
+        public long Gid { get; private set; }
+
+        public ushort Address { get; private set; }
+
+        public RemotePointType Type { get; private set; }
+    }
+}
---
+++ DERMS/FieldProcessor/Model/RemotePointType.cs
@@ -0,0 +1,10 @@
+﻿namespace FieldProcessor.Model
+{
+    public enum RemotePointType : ushort
+    {
+        DiscreteInput = 0,
+        Coil = 1,
+        InputRegister = 2,
+        HoldingRegister = 3,
+    }
+}
