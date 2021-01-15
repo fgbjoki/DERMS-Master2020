3ca2341aaee3f0c0a31c2136edc51a8ab81153ef
Aleksandar Jovanov <jovanov33@gmail.com>
2021-01-15 14:19:05 +0100

    [NMS] Discrete now has currentValue and switch has normalOpen model code

--- DERMS/NetworkManagementService/DataModel/Wires/Switch.cs
+++ DERMS/NetworkManagementService/DataModel/Wires/Switch.cs
@@ -1,4 +1,6 @@
-﻿using NetworkManagementService.DataModel.Core;
+﻿using Common.AbstractModel;
+using Common.GDA;
+using NetworkManagementService.DataModel.Core;
 
 namespace NetworkManagementService.DataModel.Wires
 {
@@ -11,7 +13,58 @@ namespace NetworkManagementService.DataModel.Wires
 
         protected Switch(Switch copyObject) : base(copyObject)
         {
+            NormalOpen = copyObject.NormalOpen;
+        }
 
+        public bool NormalOpen { get; set; }
+
+        public override bool HasProperty(ModelCode property)
+        {
+            switch (property)
+            {
+                case ModelCode.SWITCH_NORMALOPEN:
+                    return true;
+                default:
+                    return base.HasProperty(property);
+            }
+        }
+
+        public override void GetProperty(Property property)
+        {
+            switch (property.Id)
+            {
+                case ModelCode.SWITCH_NORMALOPEN:
+                    property.SetValue(NormalOpen);
+                    break;
+                default:
+                    base.GetProperty(property);
+                    break;
+            }
+        }
+
+        public override void SetProperty(Property property)
+        {
+            switch (property.Id)
+            {
+                case ModelCode.SWITCH_NORMALOPEN:
+                    NormalOpen = property.AsBool();
+                    break;
+                default:
+                    base.SetProperty(property);
+                    break;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override bool Equals(object x)
+        {
+            Switch compareObject = x as Switch;
+
+            return compareObject != null && base.Equals(x) && NormalOpen == compareObject.NormalOpen;
         }
     }
 }
