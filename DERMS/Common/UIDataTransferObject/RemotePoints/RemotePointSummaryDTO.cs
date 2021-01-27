c7087b4ab689c96861be108f0148c5a73b256ce0
Aleksandar Jovanov <jovanov33@gmail.com>
2021-01-27 19:57:00 +0100

    [Common] Added DTOs for UIClient

---
+++ DERMS/Common/UIDataTransferObject/RemotePoints/RemotePointSummaryDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+
+namespace Common.UIDataTransferObject.RemotePoints
+{
+    [DataContract]
+    [KnownType(typeof(AnalogRemotePointSummaryDTO))]
+    [KnownType(typeof(DiscreteRemotePointSummaryDTO))]
+    public abstract class RemotePointSummaryDTO
+    {
+        [DataMember]
+        public long GlobalId { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public int Address { get; set; }
+    }
+}
