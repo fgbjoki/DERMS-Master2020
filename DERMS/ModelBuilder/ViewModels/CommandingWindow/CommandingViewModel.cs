838225a5941292aa5719614fcc983bd647b1648c
Aleksandar Jovanov <jovanov33@gmail.com>
2021-01-28 18:07:44 +0100

    [ClientUI] Commanding window now has close button

--- DERMS/ModelBuilder/ViewModels/CommandingWindow/CommandingViewModel.cs
+++ DERMS/ModelBuilder/ViewModels/CommandingWindow/CommandingViewModel.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using ClientUI.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace ClientUI.ViewModels.CommandingWindow
 {
@@ -11,8 +14,18 @@ namespace ClientUI.ViewModels.CommandingWindow
         public CommandingViewModel(string title)
         {
             Title = title;
+
+            CloseWindowCommand = new RelayCommand(CloseWindow, null);
         }
 
         public string Title { get; set; }
+
+        public ICommand CloseWindowCommand { get; set; }
+
+        public void CloseWindow(object param)
+        {
+            Window window = param as Window;
+            window.Close();
+        }
     }
 }
