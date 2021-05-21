94ab0f93d696a504c0da4a6fdba29c51c6f3ed13
Aleksandar Jovanov <jovanov33@gmail.com>
2021-05-21 09:59:42 +0200

    [NMS] Out of buffer memory on WCF

--- DERMS/NetworkManagementService/Program.cs
+++ DERMS/NetworkManagementService/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Common.ServiceInterfaces;
+using System;
 using System.ServiceModel;
 
 namespace NetworkManagementService
@@ -11,6 +12,10 @@ namespace NetworkManagementService
             NetworkModel nms = new NetworkModel();
 
             ServiceHost host = new ServiceHost(nms);
+            var binding = new NetTcpBinding();
+            binding.MaxBufferSize = 2147483647;
+            binding.MaxReceivedMessageSize = 2147483647;
+            host.AddServiceEndpoint(typeof(INetworkModelDeltaContract), binding, "net.tcp://localhost:11112/NetworkModel/INetworkModelDeltaContract");
             host.Open();
 
             Console.WriteLine("Service started...");
